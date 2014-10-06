14a5d3e97e77e7b34f3d05427c879aca7783a372
mautematico <mauricio@navarromiranda.mx>
2014-10-05 22:24:20 -0500

    Agrego algunas pruebas

--- P_0110/Reloj/src/PruebaReloj.java
+++ P_0110/Reloj/src/PruebaReloj.java
@@ -13,6 +13,39 @@
 public class PruebaReloj {
     public static void main (String [] args) {
         
+        System.out.println("Reloj con constructor sin parámetros:");
+        Reloj reloj = new Reloj();
+        System.out.println(reloj);
+        
+        System.out.println("Reloj con Reloj(23):");
+        reloj = new Reloj(23);
+        System.out.println(reloj);
+
+        System.out.println("Reloj con Reloj(2,17):");
+        reloj = new Reloj(2,17);
+        System.out.println(reloj);
+
+        System.out.println("Reloj con Reloj(2,17,13):");
+        reloj = new Reloj(2,17,13);
+        System.out.println(reloj);
+        
+        
+        reloj.incrementarSegundo();
+        System.out.println(reloj);
+
+        reloj.incrementarSegundo(787219);
+        System.out.println(reloj);
+
+        reloj.incrementarMinuto();
+        System.out.println(reloj);
+        reloj.incrementarMinuto(5876);
+        System.out.println(reloj);
+
+        reloj.incrementarHora();
+        System.out.println(reloj);
+        reloj.incrementarHora(47857);
+        System.out.println(reloj);
+
         Reloj reloj2 = new Reloj(11, 59, 56);
        
         for (int i=1; i>=1; i++) {
