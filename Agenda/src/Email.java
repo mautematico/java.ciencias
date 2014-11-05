390052f872622a0a693d01d9ec3ed763b632c98b
mautematico <mauricio@navarromiranda.mx>
2014-11-04 21:59:55 -0600

    Creo constructores para Email, Muevo los atributos al inicio de la clase (Email)

--- Agenda/src/Email.java
+++ Agenda/src/Email.java
@@ -5,17 +5,31 @@ import java.util.*;
  * 
  */
 public class Email {
+    /**
+     * 
+     */
+    private String correo;
+    private int tipo;
 
     /**
      * @param correo: correo
      */
-    public Email(String correo) {
+    public Email(String correo, int tipo) {
         this.correo = correo;
+        
+        if(0 <= tipo && tipo <= 3){
+            this.tipo = tipo;
+        }
+        else{
+            this.tipo = 0;
+        }
     }
-    public Email(){
-
+    
+    public Email(String correo){
+        this(correo,0);
     }
-
+    
+    
     public String getCorreo() {
         return correo;
     }
@@ -23,11 +37,4 @@ public class Email {
     public void setCorreo(String correo) {
         this.correo = correo;
     }
-
-    /**
-     * 
-     */
-    private String correo;
-
-
 }
