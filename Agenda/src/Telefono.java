ca55e263e3a3d0a99852a32f5af41d95afb897b3
GABI <gabriiela_guthiierrez@hotmail.com>
2014-11-04 21:53:10 -0600

    cree constructores, uno con dos parametros (un numero de tipo string y un tipo de tipo int) y otro con un solo parametro (un numero de tipo string)

--- Agenda/src/Telefono.java
+++ Agenda/src/Telefono.java
@@ -6,15 +6,22 @@ import java.util.*;
  */
 public class Telefono {
     
-    private String Numero;
+    private String numero;
+    private int tipo;
 
-    public Telefono(String telefono) {
-        
+    public Telefono(String numero, int tipo) {
+        if(0<=tipo && tipo<=3){
+            this.tipo = tipo;
+        }
+        else{
+            this.tipo = 0;
+        }
+        this.numero = numero;
     }
-
-    /**
-     * 
-     */
+    
+    public Telefono(String numero){
+        this(numero,0);
+    }
 
     
 
