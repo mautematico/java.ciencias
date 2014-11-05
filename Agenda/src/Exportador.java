b3541ba0902d6b0b6320f4ba9c070172fd193cf4
GABI <gabriiela_guthiierrez@hotmail.com>
2014-11-05 01:24:42 -0600

    agregue clases exportadorr e importador para leer y escribir archivos

---
+++ Agenda/src/Exportador.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author GABI
+ */
+import java.io.*;
+public class Exportador {
+    public static void escribir ( String nombreDeArchivo){
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("c:/prueba.txt");
+            pw = new PrintWriter(fichero);
+ 
+            for (int i = 0; i < 10; i++)
+                pw.println("Linea " + i);
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+    }
+}
---
+++ Agenda/src/Importador.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author GABI
+ */
+import java.io.*;
+
+public class Importador {
+    public static String leer (String nombreDeArchivo){
+      File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+      String cadena = "";
+ 
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File ("C:\\archivo.txt");
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+ 
+         // Lectura del fichero
+         String linea;
+         while((linea=br.readLine())!=null)
+            cadena += linea;
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta
+         // una excepcion.
+         try{                   
+            if( null != fr ){  
+               fr.close();    
+            }                 
+         }catch (Exception e2){
+            e2.printStackTrace();
+         }
+      }
+      return cadena;
+    }
+}
