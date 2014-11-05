f70a69e4b4b6b22338aee599d091e5a1cfbc0a15
mautematico <mauricio@navarromiranda.mx>
2014-11-04 20:30:27 -0600

    Agrego get, set y constructores para Contacto.

--- Agenda/src/Email.java
+++ Agenda/src/Email.java
@@ -7,15 +7,27 @@ import java.util.*;
 public class Email {
 
     /**
-     * 
+     * @param correo: correo
      */
-    public Email() {
+    public Email(String correo) {
+        this.correo = correo;
+    }
+    public Email(){
+
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
     /**
      * 
      */
-    private String Correo;
+    private String correo;
 
 
 }
