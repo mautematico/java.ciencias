b7f65032190150e4b396c412fe9b4d747fd363bc
mautematico <mauricio@navarromiranda.mx>
2014-11-08 22:39:56 -0600

    Retiro un import que no usabamos

--- Agenda/src/Ventana.java
+++ Agenda/src/Ventana.java
@@ -1,5 +1,4 @@
 //Hecho por: Arévalo Loyola Alma Rosario, Gutiérrez Guillén Gabriela Jocelyn, Navarro Miranda Mauricio
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
