d466825347d0467e3df0395b4770592f7154e866
GABI <gabriiela_guthiierrez@hotmail.com>
2014-11-08 19:18:46 -0600

    listos todos los comentarios del mundo :P

--- Agenda/src/Telefono.java
+++ Agenda/src/Telefono.java
@@ -1,3 +1,4 @@
+//Hecho por: Arévalo Loyola Alma Rosario, Gutiérrez Guillén Gabriela Jocelyn, Navarro Miranda Mauricio
 import java.io.Serializable;
 import java.util.*;
 
@@ -6,9 +7,11 @@ import java.util.*;
  */
 public class Telefono implements Serializable {
 
+//Creamos variables de instancia para la clase telefono
     private String numero;
     private int tipo;
 
+//creamos un constructor que recibe un numero y un tipo y verificamos el tipo
     public Telefono(String numero, int tipo) {
         if (0 <= tipo && tipo <= 3) {
             this.tipo = tipo;
@@ -17,11 +20,13 @@ public class Telefono implements Serializable {
         }
         this.numero = numero;
     }
-
+    
+//Creamos un constructor que asigna un tipo determinado
     public Telefono(String numero) {
         this(numero, 0);
     }
 
+//creamos metodos accesores para esta clase
     public String getNumero() {
         return numero;
     }
@@ -38,6 +43,7 @@ public class Telefono implements Serializable {
         this.tipo = tipo;
     }
 
+//Sobreescribimos el metodo toString
     @Override
     public String toString() {
         switch (tipo) {
