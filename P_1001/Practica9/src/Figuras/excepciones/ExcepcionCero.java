3f1becb521b946fe16c0a89bcb4ee416e1c5b41e
mautematico <mauricio@navarromiranda.mx>
2014-11-09 20:55:21 -0600

    Actualizo la autoría de las clases de excepción, son de Armando.

--- P_1001/Practica9/src/Figuras/excepciones/ExcepcionCero.java
+++ P_1001/Practica9/src/Figuras/excepciones/ExcepcionCero.java
@@ -3,16 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Figuras.excepciones;
+package figuras.excepciones;
 
 /**
- *
- * @author GABI
+ * Clase que extiende a Exception
+ * @author Armand
  */
-public class ExcepcionCero extends RuntimeException {
-    
+public class ExcepcionCero extends Exception {
+
     public ExcepcionCero() {
-        super("El valor ingresado es 0");
+        //Se construye la excepción llamando al constructor de la superclase
+        //que recibe un String, dicho String es un mensaje que ayuda a entender 
+        //mejor la causa del error
+        super("El número es 0");
     }
     
 }
--- P_1001/Practica9/src/Figuras/excepciones/ExcepcionNumeroNegativo.java
+++ P_1001/Practica9/src/Figuras/excepciones/ExcepcionNumeroNegativo.java
@@ -3,16 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Figuras.excepciones;
+package figuras.excepciones;
 
 /**
- *
- * @author GABI
+ * Clase que extiende a ArithmeticException, que es una RuntimeException, que 
+ * a su vez es una Exception
+ * @author Armand
  */
-public class ExcepcionNumeroNegativo extends RuntimeException {
+public class ExcepcionNumeroNegativo extends ArithmeticException {
+
+    public ExcepcionNumeroNegativo() {
+        //Se construye la excepción llamando al constructor de la superclase
+        //que recibe un String, dicho String es un mensaje que ayuda a entender 
+        //mejor la causa del error
+        super("El número es negativo");
+    }
     
-   public ExcepcionNumeroNegativo(){
-        super("El valor ingresado es negativo");
-   }
-   
 }
