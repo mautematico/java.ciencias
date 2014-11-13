3eaea2f986810ca7a2cd162640591f35d7b9a229
mautematico <mauricio@navarromiranda.mx>
2014-11-13 14:35:32 -0600

    Muevo Escaque y Tablero a un nuevo paquete

--- Ajedrez/Ajedrez/src/ajedrez/juego/Escaque.java
+++ Ajedrez/Ajedrez/src/ajedrez/juego/Escaque.java
@@ -2,7 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ajedrez.piezas;
+package ajedrez.juego;
+
+import ajedrez.piezas.Pieza;
 
 /**
  *
