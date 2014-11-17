f2447a2c663a248c6d4f44b750ab46891bd4f0b4
GABI <gabriiela_guthiierrez@hotmail.com>
2014-11-17 14:23:51 -0600

    cambio cosas con eel peon :)

--- Ajedrez/Ajedrez/src/ajedrez/piezas/Peon.java
+++ Ajedrez/Ajedrez/src/ajedrez/piezas/Peon.java
@@ -13,28 +13,50 @@ public class Peon extends Pieza {
 
     @Override
     boolean[][] posicionesPosibles() {
-        boolean [][] posicionesPosibles = new boolean [8][8];
+        boolean[][] posicionesPosibles = new boolean[8][8];
         int x = this.getPosicionActual().getX();
         int y = this.getPosicionActual().getY();
-        if(0<=y+1 && y+1<=7)
-            posicionesPosibles[x][y+1]= true;
-        
+        boolean equipo = this.isEquipo();
+        if (equipo == true) {
+            if (0 <= y + 1 && y + 1 <= 7) {
+                posicionesPosibles[x][y + 1] = true;
+            }
+        } else {
+            if (0 <= y - 1 && y - 1 < -7) {
+                posicionesPosibles[x][y - 1] = true;
+            }
+        }
+
         posicionesPosibles[x][y] = false;
         return posicionesPosibles;
     }
-    
-    boolean [][] posicionesParaComer(){
-        boolean [][] posicionesParaComer = new boolean [8][8];
+
+    boolean[][] posicionesParaComer() {
+        boolean[][] posicionesParaComer = new boolean[8][8];
         int x = this.getPosicionActual().getX();
         int y = this.getPosicionActual().getY();
-        if(0<=y+1 && y+1<=7){
-            if(0<=x+1 && x+1<=7)
-                posicionesParaComer[x+1][y+1] = true;
-            if(0<=x-1 && x-1<=7)
-                posicionesParaComer[x-1][y+1] = true;
+        boolean equipo = this.isEquipo();
+        if (equipo == true) {
+            if (0 <= y + 1 && y + 1 <= 7) {
+                if (0 <= x + 1 && x + 1 <= 7) {
+                    posicionesParaComer[x + 1][y + 1] = true;
+                }
+                if (0 <= x - 1 && x - 1 <= 7) {
+                    posicionesParaComer[x - 1][y + 1] = true;
+                }
+            }
+        } else {
+            if (0 <= y - 1 && y - 1 <= 7) {
+                if (0 <= x + 1 && x + 1 <= 7) {
+                    posicionesParaComer[x + 1][y - 1] = true;
+                }
+                if (0 <= x - 1 && x - 1 <= 7) {
+                    posicionesParaComer[x - 1][y - 1] = true;
+                }
+            }
         }
+
         return posicionesParaComer;
     }
 
-    
 }
