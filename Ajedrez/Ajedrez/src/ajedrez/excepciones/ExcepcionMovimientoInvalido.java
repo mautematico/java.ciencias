e39e7cf19c3129c51f6413968042f1f9251187cb
mautematico <mauricio@navarromiranda.mx>
2014-11-18 14:24:46 -0600

    creo la excepcion movimiento invalido aunq no se si realmente extienda a exception o a runtime

---
+++ Ajedrez/Ajedrez/src/ajedrez/excepciones/ExcepcionMovimientoInvalido.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrez.excepciones;
+
+/**
+ *
+ * @author actuaria
+ */
+public class ExcepcionMovimientoInvalido extends Exception {
+   public ExcepcionMovimientoInvalido(){
+       super("Movimiento invalido");
+   }
+}
